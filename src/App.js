396b968612ad8adde883a25b81cecd040df7bf81
Izirae <lautaro.bazzola@gmail.com>
2022-06-02 21:47:49 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          banana
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=cNPGDx7nBw4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leclerc
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
